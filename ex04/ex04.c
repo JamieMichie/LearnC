88efb160e7a6653760a178a741da9acbbc2cf58f
Jampot <jamie.michie@googlemail.com>
2021-09-14 19:19:38 +0100

    broken code

--- ex04/ex04.c
+++ ex04/ex04.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void test(void);
+
+void test(void){
+	printf("Test function : insert breakpoint here\n");
+	for(int j = 0; j < 100; j++){
+		printf("Test function : insert breakpoint in here too?\n");
+	}
+}
 int main(int argc, char *argv[]){
+	test();
+	for(int i = 0; i <= 100; i++){
+		printf("Argv :  %s \n",argv[i]);
+		//printf(argc);
+		printf("i = %d \n", i);
+	}
 	return 0;
 }
