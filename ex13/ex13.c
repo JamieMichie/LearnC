4c48bf275c9ce40008c3f0eb0f8adb7f3caa590c
Jampot <jamie.michie@googlemail.com>
2021-09-23 19:40:42 +0100

    clean up

---
+++ ex13/ex13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	for(int i = 0; i < argc; i++){
+		printf("arg %d : %s \n", i, argv[i]);
+	}
+
+	char *states[] = {"California", "Oregon", "Washington", "Texas"};
+	
+	int num_states = 4;
+
+	for(int i =0; i <  num_states; i++){
+		printf("State %d : %s \n", i, states[i]);
+	}
+	return 0;
+}
